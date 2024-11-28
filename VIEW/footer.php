41e9caceff4ef0d8115710a00fdb43273b76268b
efrei-tc <titouan.charlet@efrei.net>
2024-11-28 10:25:11 +0100

    maj header/footer

--- VIEW/footer.php
+++ VIEW/footer.php
@@ -1,2 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="?page=accueil" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+      <li class="nav-item"><a href="?page=produits" class="nav-link px-2 text-body-secondary">Vélos</a></li>
+      <li class="nav-item"><a href="?page=contact" class="nav-link px-2 text-body-secondary">Contact</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">Smartbike, 32 Avenue de la république, Villejuif, 94800 – Tél. : 01 23 45 67 89</p>
+  </footer>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
     session_start();
     include 'view/header.php';
     include 'controller/route.php';
     include 'view/footer.php';
-
 ?>
