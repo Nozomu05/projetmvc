c659328fac32d2603f724ace702124e5b1faf030
unknown <giulianp@orange.fr>
2024-11-28 12:01:04 +0100

    contact

---
+++ VIEW/contact.php
@@ -0,0 +1,11 @@
+<h1>Contact</h1>
+
+<form action="" method="post">
+
+    Nom : <input type="text" name="nom" required> <br>
+    Prénom : <input type="text" name="prenom" required><br>
+    Email : <input type="email" name="email" required> <br>
+    Message : <input type="password" name="mdp" required> <br>
+    <button>Envoyer</button>
+
+</form>
