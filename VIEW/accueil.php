dabff72b5a445c995c16c99a21334fd2d9749670
Nozomu05 <tranminhhoanganh2005@gmail.com>
2024-11-28 11:38:20 +0100

    changement du accueil

--- VIEW/accueil.php
+++ VIEW/accueil.php
@@ -1,14 +1,27 @@
-<h1>accueil</h1>
-<h2><?= $lastarticle['nom_produit'] ?></h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+</body>
+    <h1>accueil</h1>
+    <h2><?= $lastarticle['nom_produit'] ?></h2>
 
 
-<div>
-    <img src="<?= $lastarticle['image'] ?>" alt="">
-    <p>
-        <?= $lastarticle['description'] ?>
-    </p>
+    <div>
+        <img src="<?= $lastarticle['image'] ?>" alt="">
+        <p>
+            <?= $lastarticle['description'] ?>
+        </p>
 
-    <small>Prix : <?= $lastarticle['prix'] ?></small>
-    <br>
+        <small>Prix : <?= $lastarticle['prix'] ?></small>
+        <br>
 
-</div>
+    </div>
+
+
+</body>
+</html>
